1c861da3ec051df435e703ebceac1b32a55ebad2
Anderson Mello <91663226+andm3ii0@users.noreply.github.com>
2023-12-28 00:18:55 +0000

    teste noo component App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [page, setPage] = useState("home");
+
+  const onHandleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setPage(event.target.dataset.name);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div>
+      <header>
+        <p className="header-option" data-name="menu" onClick={onHandleClick}>
+          menu
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <p
+          className="header-option"
+          data-name="inventario"
+          onClick={onHandleClick}
         >
-          Learn React
-        </a>
+          inventario
+        </p>
+        <p
+          className="header-option"
+          data-name="pedidos"
+          onClick={onHandleClick}
+        >
+          pedidos
+        </p>
       </header>
+      <div>{`Você está na tela de ${page}`}</div>
     </div>
   );
 }
